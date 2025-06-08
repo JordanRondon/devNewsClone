5e0681c7586b63af59af1d9910f0bc36ce39150c
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-06-08 01:47:26 -0500

    feat: structure of the article detail page

--- src/app/article/[id]/page.jsx
+++ src/app/article/[id]/page.jsx
@@ -1,6 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
 
-export default function articlePage ( {params} ) {
-    return (
-        <div> {params.id} </div>
-    );
+async function loadArticleDetail(articleId) {
+    const res = await fetch(`https://dev.to/api/articles/${articleId}`);
+    const data = await res.json();
+    return data;
+}
+
+export default async function articlePage ( {params} ) {
+
+    const articleDetail = await loadArticleDetail(params.id);
+
+    return (<>
+        <Image
+            src={articleDetail.social_image}
+            alt=""
+            width={800}
+            height={800}
+        />
+        <Link 
+            href={articleDetail.canonical_url}
+            target="_blank">
+                go there
+        </Link>
+        <h1>
+            {articleDetail.title}
+        </h1>
+        <div dangerouslySetInnerHTML={{ __html: articleDetail.body_html }} />
+    </>);
 }
