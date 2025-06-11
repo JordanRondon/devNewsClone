52a479b034a1c431ef95da2ff57aaf239902c5ab
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-06-11 02:24:23 -0500

    feat: integration of loading and error pages

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center flex-col">
+      <h1 className="text-4xl font-bold">404 - Page not found</h1>
+      <p className="text-gray-500 mt-2">
+        We couldn't find what you were looking for.
+      </p>
+    </div>
+  );
+}
